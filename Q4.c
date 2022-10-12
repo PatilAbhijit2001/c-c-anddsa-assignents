5783aa2a8085e894da3160a5cf906f8e0b41acdb
PatilAbhijit2001 <115525685+PatilAbhijit2001@users.noreply.github.com>
2022-10-12 19:23:20 +0530

    Update Q4.c

--- Q4.c
+++ Q4.c
@@ -5,7 +5,7 @@
         float a;
         printf("Enter the radius of circle\n");
         scanf("%f", &a);
-        //here we use %-0.3f for printing only 3 digits after decimal
-        printf("Area of circle is %-0.3f having the radius %-0.3f",3.14*a*a,a);
+        //here we use %0.3f for printing only 3 digits after decimal
+        printf("Area of circle is %0.3f having the radius %0.3f",3.14*a*a,a);
         return 0;
     }
