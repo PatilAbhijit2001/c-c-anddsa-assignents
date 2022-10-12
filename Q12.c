ed70798ffe77208f622503a5c5f6b2145ef66ed9
PatilAbhijit2001 <115525685+PatilAbhijit2001@users.noreply.github.com>
2022-10-12 18:36:03 +0530

    Add files via upload

---
+++ Q12.c
@@ -0,0 +1,7 @@
+int main()
+{
+    int x = printf("ineuron");
+    printf("%d",x);
+    //the output of this program is ineuron7 
+    //it means it is a length of string using printf function program
+}
