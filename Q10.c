4b9a999e00bb12f90b14892ceeb03c757469aed6
PatilAbhijit2001 <115525685+PatilAbhijit2001@users.noreply.github.com>
2022-10-15 10:39:58 +0530

    Add files via upload

---
+++ Q10.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+    int main()
+    {
+        int D,M,Y;
+        printf("Enter a date -\n");
+        scanf("%d/%d/%d",&D,&M,&Y);
+        printf("%d/%d/%d",D,M,Y);
+        return 0;
+    }
