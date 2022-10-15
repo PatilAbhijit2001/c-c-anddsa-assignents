d40fd5fabdc2a2cecbaaeae39f0066e29534a452
PatilAbhijit2001 <115525685+PatilAbhijit2001@users.noreply.github.com>
2022-10-15 10:46:38 +0530

    Add files via upload

---
+++ Q11.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+    int main()
+    {
+        int H,M;
+        printf("Enter the time in the form of HH:MM-\n");
+        scanf("%d:%d",&H,&M);
+        printf("%d hour and %d minute",H,M);
+        return 0;
+
+    }
